074a2851a4511390d3bab6745afdf9d73f8313b8
Beinsezii <beinsezii@gmail.com>
2019-03-08 17:22:40 -0800

    Style improvements

--- bszgw.py
+++ bszgw.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""A GTK simplifier."""
+"""MAIN STRING TODO"""
+from gi.repository import Gtk
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
 
 
 
 
 class App(Gtk.Window):
+    """DOCSTRING TODO
+EXPERIMENTAL"""
     def __init__(self, label, widget):
         super(App, self).__init__()
         self.connect("destroy", Gtk.main_quit)
@@ -20,28 +22,39 @@ class App(Gtk.Window):
         for x in prelaunch:
             if len(x) == 1:
                 x[0]()
+
             else:
                 x[0](*x[1:])
+
         Gtk.main()
 
 
 
 
 def AutoBox(big_list, vspacing = 5, hspacing = 15, orientation=Gtk.Orientation.VERTICAL):
+    """DOCSTRING TODO
+EXPERIMENTAL"""
     sub_orientation = 1 if orientation == 0 else 0
     box = Gtk.Box.new(orientation, vspacing if orientation==Gtk.Orientation.VERTICAL else hspacing)
 
     for x in big_list:
         if isinstance(x, (list, tuple)):
             x = AutoBox(x, vspacing, hspacing, sub_orientation)
+
         elif isinstance(x, str):
             dimensions = x.casefold().split('x')
             x = Gtk.Label.new(None)
             x.set_size_request(int(dimensions[0]), int(dimensions[1]))
-        if len(big_list) == 1: return x
-        if x != None: box.pack_start(x, True, True, 0)
 
-    if not box.get_children(): return None
+        if len(big_list) == 1:
+            return x
+
+        if x != None:
+            box.pack_start(x, True, True, 0)
+
+    if not box.get_children():
+        return None
+
     return box
 
 
@@ -63,40 +76,49 @@ class Adjuster(Gtk.Box):
 
         if slider:
             self.slider = Gtk.Scale.new(orientation, self.adjustment)
-            self.slider.props.draw_value=not spinner
-            self.slider.props.digits=self.decimals
+            self.slider.props.draw_value = not spinner
+            self.slider.props.digits = self.decimals
+
             if orientation == Gtk.Orientation.HORIZONTAL:
                 width = slider_size
                 height = -1
+
             else:
-                self.slider.props.value_pos=Gtk.PositionType.LEFT
+                self.slider.props.value_pos = Gtk.PositionType.LEFT
                 width = -1
                 height = slider_size
+
             self.slider.set_size_request(width, height)
             self.adjuster_box.pack_start(self.slider, True, True, 0)
+
             if tooltip:
-                self.slider.props.tooltip_text=tooltip
+                self.slider.props.tooltip_text = tooltip
 
         if spinner:
             self.spinner = Gtk.SpinButton.new(self.adjustment, step_increment, self.decimals)
             self.adjuster_box.pack_start(self.spinner, not slider, True, 0)
+
             if tooltip:
-                self.spinner.props.tooltip_text=tooltip
+                self.spinner.props.tooltip_text = tooltip
 
     @property
     def value(self):
-        return float(f'%.{self.decimals}f'%(self.adjustment.props.value)) if self.decimals > 0 else int(f'%.{self.decimals}f'%(self.adjustment.props.value))
+        if self.decimals > 0:
+            return float(f'%.{self.decimals}f'%(self.adjustment.props.value))
+
+        else:
+            return int(f'%.{self.decimals}f'%(self.adjustment.props.value))
 
     @value.setter
     def value(self, new_value):
-        self.adjustment.props.value=new_value
+        self.adjustment.props.value = new_value
 
 
 
 
 class CheckBox(Gtk.CheckButton):
-    """Basically just a normal GTK checkbutton with the 'value' variable I like to use.
-    Possibly overkill."""
+    """Basically just a normal GTK checkbutton with the 'value' property and other tiny additions.
+Possibly overkill."""
     def __init__(self, label, value, tooltip=None):
         super(CheckBox, self).__init__(label)
         if tooltip:
@@ -130,13 +152,16 @@ class DropDown(Gtk.ComboBoxText):
                 self.append_text(str(x[0]))
             else:
                 self.append_text(str(x))
-        self.value=value
+        self.value = value
 
     @property
     def value(self):
-        if self.get_active() == -1: return None
+        if self.get_active() == -1:
+            return None
+
         if self.enums:
             return self.values[self.get_active()]
+
         else:
             return self.get_active()
 
@@ -144,6 +169,7 @@ class DropDown(Gtk.ComboBoxText):
     def value(self, new_value):
         if self.enums:
             self.set_active(self.values.index(new_value))
+
         else:
             self.set_active(new_value)
 
@@ -153,7 +179,7 @@ class DropDown(Gtk.ComboBoxText):
 class RadioButtons(Gtk.Box):
     """DOCSTRING TODO"""
     def __init__(self, label, buttons, value, orientation=Gtk.Orientation.VERTICAL, enums=False, tooltip=None):
-        assert len(buttons)>1
+        assert len(buttons) > 1
         super(RadioButtons, self).__init__()
         self.set_orientation(Gtk.Orientation.VERTICAL)
         self.enums = enums
@@ -163,15 +189,21 @@ class RadioButtons(Gtk.Box):
         self.pack_start(self.buttons_box, True, True, 0)
         self.radio_buttons = []
         if self.enums:
-            self.values=[]
+            self.values = []
         for num, var in enumerate(buttons):
-            self.radio_buttons.append(Gtk.RadioButton.new_with_label(None, str(var) if not self.enums else str(var[0])))
+            self.radio_buttons.append(Gtk.RadioButton.new_with_label(
+                None, str(var) if not self.enums else str(var[0])))
             self.buttons_box.pack_start(self.radio_buttons[num], True, True, 0)
-            if num>0:
+
+            if num > 0:
                 self.radio_buttons[num].join_group(self.radio_buttons[0])
+
             if self.enums:
                 self.values.append(var[1])
-            if tooltip: self.radio_buttons[num].set_tooltip_text(tooltip)
+
+            if tooltip:
+                self.radio_buttons[num].set_tooltip_text(tooltip)
+
         self.value = value
 
     @property
@@ -180,6 +212,7 @@ class RadioButtons(Gtk.Box):
             if x.get_active():
                 if self.enums:
                     return self.values[self.radio_buttons.index(x)]
+
                 else:
                     return self.radio_buttons.index(x)
 
@@ -190,6 +223,7 @@ class RadioButtons(Gtk.Box):
                 if self.values[self.radio_buttons.index(x)] == new_value:
                     x.set_active(1)
                     return
+
             else:
                 if self.radio_buttons.index(x) == new_value:
                     x.set_active(1)
@@ -199,23 +233,30 @@ class RadioButtons(Gtk.Box):
 
 
 class TextBox(Gtk.Box):
-    def __init__(self, label, value, multi_line = True, size="200x100"):
+    """DOCSTRING TODO"""
+    def __init__(self, label, value, multi_line=True, size="200x100"):
         super(TextBox, self).__init__()
         self.multi_line = multi_line
         self.set_orientation(Gtk.Orientation.VERTICAL)
         self.label = Gtk.Label.new(label)
         self.pack_start(self.label, False, True, 0)
+
         if self.multi_line:
             self.text_box = Gtk.TextView.new()
+
         else:
             self.text_box = Gtk.Entry.new()
+
         self.text_buffer = self.text_box.get_buffer()
         self.scroll_box = Gtk.ScrolledWindow.new(None, None)
         self.scroll_box.add(self.text_box)
         self.pack_start(self.scroll_box, True, True, 0)
         sizes = size.casefold().split("x")
         self.scroll_box.set_min_content_width(int(sizes[0]))
-        if self.multi_line: self.scroll_box.set_min_content_height(int(sizes[1]))
+
+        if self.multi_line:
+            self.scroll_box.set_min_content_height(int(sizes[1]))
+
         self.value = value
 
     @property
@@ -223,7 +264,9 @@ class TextBox(Gtk.Box):
         if self.multi_line:
             bounds = self.text_buffer.get_bounds()
             return self.text_buffer.get_text(bounds[0], bounds[1], True)
-        else: return self.text_buffer.get_text()
+
+        else:
+            return self.text_buffer.get_text()
 
     @value.setter
     def value(self, new_value):
